148dfd1fd376a0761ef994f740727528587a8030
scotty974 <fabienetheve@hotmail.com>
2023-02-15 20:05:46 +0100

    Mise en place du projet

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-12" id="getSong">
+			<form>
+				<label for="formFileLg" class="form-label">Large file input example</label>
+				<input class="form-control form-control-lg" id="formFileLg" type="file">
+			</form>
+		</div>
+	</div>
+</div>
