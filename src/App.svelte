4cbc99e684acfa84d00c2ce71a0c039b33b7911c
scotty974 <fabienetheve@hotmail.com>
2023-02-15 20:46:22 +0100

    Mise en page de la page d'accueil, gestion d'erreur,recupération du son

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,50 @@
+<script>
+import Three from "./three.svelte";
+let songSave = null;
+let checked = false
+let getNothing = false
+let errorMessage = ''
+function song(event){
+	event.preventDefault();
+		if (songSave) {
+			console.log(songSave);
+			checked = true;
+			console.log(checked);
+		} else {
+			getNothing =true
+			errorMessage = "Veuillez remplir le champ."
+			console.log(getNothing)
+		}
+}
+</script>
 
 
+
+{#if !checked}
 
 <div class="container">
 	<div class="row">
 		<div class="col-12" id="getSong">
-			<form>
-				<label for="formFileLg" class="form-label">Large file input example</label>
-				<input class="form-control form-control-lg" id="formFileLg" type="file">
+			<form on:submit|preventDefault={song}>
+				<label for="formFileLg" class="form-label">Mettez une musique</label>
+				<input class="form-control form-control-lg" id="formFileLg" type="file" bind:value={songSave}>
+				{#if getNothing}
+				<p class="text-danger">{errorMessage}</p>
+				{/if}
+				<button type="submit" class="my-5">Envoyer</button>
+				
 			</form>
 		</div>
 	</div>
 </div>
+	{:else if checked}
+	<div class="container">
+		<div class="row">
+			<div id="getSong">
+				<h1 class="text-light">Votre Musique : {songSave}</h1>
+				<Three />
+			</div>
+		</div>
+	</div>
+	
+{/if}
---
+++ src/three.svelte
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
